78b42514e6e08d939cf16f5312f45effcf8ee053
junheeLee96 <dlwnsgml203@naver.com>
2022-01-17 16:27:30 +0900

    movie

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
+import React from "react";
+import axios from "axios";
+import Movie from "./Movie"
 
+class App extends React.Component{
+  state = {
+    isloading:true,
+    movies: []
+  };
 
-function App() {
-  return (
-    <div>
-    </div>
-  );
+  getMovies = async () =>{
+    const {data: {data: {movies}}} = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating");
+    this.setState({movies, isloading:false})
+  }
+  componentDidMount(){
+    this.getMovies();
+  }
+  render(){
+    const {isloading, movies} = this.state;
+    return <div>{isloading ? "loading...." : movies.map(movie=>{
+      return <Movie id={movie.id} year={movie.year} title={movie.tile} summary={movie.summary} poster={movie.medium_cover_image} />
+    })
+    }</div>
+  }
+  
 }
 
 export default App;
+ 
